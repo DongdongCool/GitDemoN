3b2305fba1396edf4e5d26db01f607a9c9d6e5da
jack <550545764@qq.com>
2019-01-16 23:40:45 +0800

    只能上传到自己的 Git ,选错目录到比人的 目录 是没有权限的，导致push错误

--- 爬虫2.py
+++ 爬虫2.py
@@ -2,13 +2,17 @@
 from bs4 import BeautifulSoup
 import requests
 
-baidu = requests.get("https://www.baidu.com").content
+data = requests.get("https://www.baidu.com").content
 
-soup = BeautifulSoup(baidu,"html.parser")
+soup = BeautifulSoup(data,"html.parser")
 
 # print(soup.text)
+#
+# links = soup.findAll('a')  #  <a> 标签在 html中 是超链接的 标签，其中的内容是李恩杰
+#
+# for link in links:  #遍历所有 link
+#     print(link.string)  #只显示 link元素中的 字符串元素
 
-links = soup.findAll('a')  #  <a> 标签在 html中 是超链接的 标签，其中的内容是李恩杰
+print(soup.title.text)  # 获取网页上的 各种元素 使用 不同的标签来搜索
 
-for link in links:  #遍历所有 link
-    print(link.string)  #只显示 link元素中的 字符串元素
+# print(soup.)
